8acfb2f285d83e7255857ba17edb8e3fb33617f3
Zie Weaver <zearen.wover@gmail.com>
2015-08-01 13:47:58 -0400

    Got the structure set up

--- src/lambdapad.hs
+++
@@ -1,9 +0,0 @@
-import Game.LambdaPad ( lambdaPad, stop )
-import Game.LambdaPad.Pads.F310 ( f310 )
-import Game.LambdaPad.Games.GuildWars2 ( guildWars2 )
-
-main :: IO ()
-main = do
-  lpLoop <- lambdaPad f310 $ guildWars2 1.0
-  _ <- getLine
-  stop lpLoop
